d5e82227d56796c83128498b1f75c9cba5ff4469
eviry-ishikawa <ishikawa@eviry.com>
2020-11-07 17:23:38 +0900

    modify index.tsx

--- src/pages/[articleId]/index.tsx
+++ src/pages/[articleId]/index.tsx
@@ -1,22 +1,7 @@
 import { NextPage } from 'next'
-import { useGetArticlesQuery } from '../../generated/graphql'
 
 const ArticlePage: NextPage = () => {
-  const { loading, error, data } = useGetArticlesQuery()
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (loading) {
-    return <div>Error...</div>
-  }
-
-  return <div>{data.articles.map((article) => {
-    <div>
-      {article.content}
-    </div>
-  })}</div>
+  return <div></div>
 }
 
 export default ArticlePage
