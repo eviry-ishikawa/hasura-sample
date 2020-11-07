9d9971054a2641693ba30c87a1474477b2981d9d
eviry-ishikawa <ishikawa@eviry.com>
2020-11-07 11:26:53 +0900

    delete files for handson

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,7 @@
 import { NextPage } from 'next'
-import Link from 'next/link'
-import { useGetAllArticlesQuery } from '../generated/graphql'
 
 const Index: NextPage = () => {
-  const { data, loading, error } = useGetAllArticlesQuery()
-
-  if (loading) {
-    return <div>...loading</div>
-  }
-
-  if (error) {
-    return <div>{error}</div>
-  }
-
-  return (
-    <div>
-      <h1>ここはトップページ</h1>
-      <Link href="post">投稿画面</Link>
-      <div>
-        {data.article.map((article) => (
-          <div>
-            <Link href={`/${article.user.displayId}/${article.id}`}>
-              {article.subject}
-            </Link>
-            <div>{article.content}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Index
