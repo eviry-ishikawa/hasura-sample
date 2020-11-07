4ca69628f53d9f21394b80abaddafed0f6109785
eviry-ishikawa <ishikawa@eviry.com>
2020-11-07 17:01:59 +0900

    add article page

---
+++ src/pages/[articleId]/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next'
+import { useGetArticlesQuery } from '../../generated/graphql'
+
+const ArticlePage: NextPage = () => {
+  const { loading, error, data } = useGetArticlesQuery()
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+export default ArticlePage
